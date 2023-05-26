5becc42ed2045bff38b43b848161fab451cbf325
maricarmendev <maricarmenra.dev@gmail.com>
2023-05-26 14:22:22 -0700

    checkpoint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,18 @@
 import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider>
       <Component {...pageProps} />
-    </SessionProvider>
+    </ClerkProvider>
   );
 };
 
